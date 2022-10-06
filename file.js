33cea6fe42ccccac9b198afc2cb632407342235d
sujanmandana <sujanmandana@gmail.com>
2022-10-06 12:50:16 +0530

    commit

---
+++ file.js
@@ -0,0 +1,21 @@
+//var total=0;
+//var count=0;
+//while(count<5)
+//{
+  //  var marks=prompt("enter the marks","")
+    //document.write("the marks obtained"+marks+"<br>")
+    //total=total+marks;
+    //count++;
+//}
+//document.write(total);
+
+let total=0;
+let count=0;
+for(let count=0;count<5;count++)
+{
+    let marks=prompt("enter the marks","")
+    document.write("the marks obtained"+marks+"<br>")
+    total=total+marks;
+    count++;
+}
+document.write(total)
